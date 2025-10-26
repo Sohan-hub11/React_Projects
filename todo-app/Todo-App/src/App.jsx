b15505bfb6835ee40ba6b332ce0a24daa5b36461
Sohan Samanta <sohansamanta18@gmail.com>
2025-10-26 14:41:00 +0530

    Updated

--- todo-app/Todo-App/src/App.jsx
+++ todo-app/Todo-App/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return <center className="todo-container">
+    <h1>Todo App</h1>
+
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">
+          <input type="text"
+            placeholder="Enter Todo here"
+          />
+        </div>
+        <div class="col-4">
+          <input type="date"
+            id="todo-date"
+          />
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-success">Add</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div class="row">
+        <div class="col-6">
+          Buy Milk
+        </div>
+        <div class="col-4">
+          26/10/2025
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div class="row">
+        <div class="col-6">
+          Go to College
+        </div>
+        <div class="col-4">
+          26/10/2025
+        </div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </center>
 }
 
 export default App
